8878daedf00346fa20f93f783ec0294d6ce9508c
Kevin Kelley <kkelley622@gmail.com>
2022-10-26 08:26:03 -0500

    commit tree 6e17ec8584dc90ada1fb53400b70c50fd320d4b2

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def badge_maker (name)
+    "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator (names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms (speakers)
+    speakers.map.with_index(1) do |speaker, room|
+        "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer (names)
+    batch_badge_creator(names).map do |badge|
+        puts badge
+    end
+    assign_rooms(names).map do |room_assign|
+        puts room_assign
+    end
+
+end
